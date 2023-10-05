16de242c189eaebbcfb15eb9bd163e4d20a79d62
elanglo1 <113066830+elanglo1@users.noreply.github.com>
2023-10-05 17:03:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+import React, {useEffect, useState, useRef} from 'react'
 
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from "./pages/HomePage.js"
+
+function App(){
+    return(
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+           
+          </ul>
+        </nav>
+      
+        <Routes>
+          <Route path="/" element={<Home />}/>
+
+          
+        </Routes>
+      
+      </>
+  
+    )
+  
+  }
+  
+  export default App;
