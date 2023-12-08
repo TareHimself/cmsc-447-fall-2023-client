2e046532e01f0fd3ab02b5d669814360037fda48
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-12-07 20:04:52 -0500

    added read me

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,9 @@
 
+// Live server
 export const ServerUrl = 'https://447-api.oyintare.dev'
 
+// Uncomment to test local debug server
 //export const ServerUrl = 'http://localhost:9500'
+
+// Uncomment to test local prod server
+//export const ServerUrl = 'http://localhost:11000'
